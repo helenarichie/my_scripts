d462758c8364979f10c14040dfd81488379b359d
helenarichie <helenarichie@gmail.com>
2022-03-28 16:04:53 -0400

    develop standalone cuda dust kernel

--- cholla/dust_model/cuda/dust_model.cu
+++ cholla/dust_model/cuda/dust_model.cu
@@ -8,14 +8,15 @@
 #include"/Users/helenraichie/GitHub/cholla/src/global/global_cuda.h"
 #include"/Users/helenraichie/GitHub/cholla/src/global/gpu.hpp"
 
+__global__ void cuda_hello() {
+    printf("Hello World from GPU!\n");
+}
+
 int main() {
     cuda_hello<<<1, 1>>>();
     return 0;
 }
 
-__global__ void cuda_hello() {
-    printf("Hello World from GPU!\n");
-}
 
 #endif // DUST_GPU
 #endif // CUDA
