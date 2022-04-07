e40d4c2cdead08da2effba92eeeae6653e4d95c6
helenarichie <helenarichie@gmail.com>
2022-04-07 17:04:21 -0400

    add compilable version of cuda dust kernel

--- cholla/dust_model/cuda/dust_model.cu
+++ cholla/dust_model/cuda/dust_model.cu
@@ -1,9 +1,11 @@
 #define CUDA
+#define SCALAR
+// #define GIT_HASH "abc"
+// #define MACRO_FLAGS "HYDRO"
 
 #include "dust_model.h"
 
 #include <cstdio>
-#include <vector>
 
 #include "/ihome/hrichie/her45/GitHub/cholla/src/global/global.h"
 #include "/ihome/hrichie/her45/GitHub/cholla/src/global/global_cuda.h"
@@ -11,42 +13,55 @@
 #include "/ihome/hrichie/her45/GitHub/cholla/src/utils/hydro_utilities.h"
 #include "/ihome/hrichie/her45/GitHub/cholla/src/utils/cuda_utilities.h"
 #include "/ihome/hrichie/her45/GitHub/cholla/src/grid/grid3D.h"
-#include "/ihome/hrichie/her45/GitHub/cholla/src/grid/grid3D.cpp"
+
+
+int n_fields = 6;
+int n_cells = 1;
+int nx = 1;
+int ny = 1;
+int nz = 1;
+int n_ghost = 2;
+int ngrid = (n_cells + TPB - 1) / TPB;
 
 int main() {
-    struct parameters Params;
-    Params.nx = 1;
-    Params.ny = 1;
-    Params.nz = 1;
-    Params.tout = 0.2;
-    Params.outstep = 0.2;
-    Params.gamma = 1.6666666666666667;
-    Params.xmin = 0.0;
-    Params.ymin = 0.0;
-    Params.zmin = 0.0;
-    Params.xlen = 1.0;
-    Params.ylen = 1.0;
-    Params.zlen = 1.0;
-    Params.xl_bcnd = 3;
-    Params.xu_bcnd = 3;
-    Params.yl_bcnd = 0;
-    Params.yu_bcnd = 0;
-    Params.zl_bcnd = 0;
-    Params.zu_bcnd = 0;
-    Params.outdir = "./";
+  Real gamma = 1.6666666666666667;
 
-    Grid3D G;
-    G.Initialize(&Params);
-    G.Set_Initial_Conditions(Params);
-    G.Set_Boundary_Conditions_Grid(Params);
+  Real rho = 1.67260e-26;
+  Real vx = 0.0;
+  Real vy = 0.0;
+  Real vz = 0.0;
+  Real P = 3.10657e-13;
+  Real rho_d = 1.67260e-26/3;
 
-    chprintf("Dimensions of each cell: dx = %f dy = %f dz = %f\n", G.H.dx, G.H.dy, G.H.dz);
-    chprintf("Ratio of specific heats gamma = %f\n", gama);
-    chprintf("Nstep = %d  Timestep = %f  Simulation time = %f\n", G.H.n_step, G.H.dt, G.H.t);
+  Real dt = 1e2;
 
+  Real host_conserved[n_fields] = {rho, vx, vy, vz, P, rho_d};
+  Real dev_conserved[n_fields];
 
-    
-    Dust_Update(C.device, H.nx, H.ny, H.nz, H.n_ghost, H.n_fields, H.dt, gamma, dev_dti_array);
+  Real host_dti_array[TPB] = {dt};
+  Real dev_dti_array[TPB];
+
+  // Memory allocation for host array
+  CudaSafeCall( cudaHostAlloc((void**)&host_conserved, n_fields*n_cells*sizeof(Real), cudaHostAllocDefault) );
+  // Memory allocation for device array
+  CudaSafeCall( cudaMalloc((void**)&dev_conserved, n_fields*n_cells*sizeof(Real)) );
+
+  // Initialize host array
+  Conserved_Init(host_conserved, rho, vx, vy, vz, P, rho_d, gamma, n_cells, nx, ny, nz, n_ghost, n_fields);
+
+  // Copy host to device
+  CudaSafeCall(cudaMemcpy(dev_dti_array, host_dti_array, ngrid*sizeof(Real), cudaMemcpyHostToDevice));
+  CudaSafeCall(cudaMemcpy(dev_conserved, host_conserved, ngrid*sizeof(Real), cudaMemcpyHostToDevice));
+
+  Dust_Update(dev_conserved, nx, ny, nz, n_ghost, n_fields, dt, gamma, dev_dti_array);
+
+  // Copy device to host
+  CudaSafeCall( cudaMemcpy(host_conserved, dev_conserved, ngrid*sizeof(Real), cudaMemcpyDeviceToHost) );
+
+  // free host and device memory
+  CudaSafeCall( cudaFreeHost(host_conserved) );
+  CudaSafeCall( cudaFree(dev_conserved) );
+
 }
 
  void Dust_Update(Real *dev_conserved, int nx, int ny, int nz, int n_ghost, int n_fields, Real dt, Real gamma, Real *dt_array) {
@@ -160,7 +175,7 @@ __global__ void Dust_Kernel(Real *dev_conserved, int nx, int ny, int nz, int n_g
      if (tid == 0) dt_array[blockIdx.x] = min_dt[0];
 }
 
-void Dust::set_tau_sp() {
+__device__ void Dust::set_tau_sp() {
   Real a1 = 1; // dust grain size in units of 0.1 micrometers
   Real d0 = n_ / (6*pow(10, -4)); // gas density in units of 10^-27 g/cm^3
   Real T_0 = 2*pow(10, 6); // K
@@ -170,6 +185,60 @@ void Dust::set_tau_sp() {
   tau_sp_ = A * (a1/d0) * (pow(T_0/T_, omega) + 1); // s
 }
 
-Real Dust::calc_dd_dt() {
+__device__ Real Dust::calc_dd_dt() {
     return -d_dust_ / (tau_sp_/3);
+}
+
+// function to initialize conserved variable array, similar to Grid3D::Constant in grid/initial_conditions.cpp 
+void Conserved_Init(Real *host_conserved, Real rho, Real vx, Real vy, Real vz, Real P, Real rho_d, Real gamma, int n_cells, int nx, int ny, int nz, int n_ghost, int n_fields)
+{
+  int i, j, k, id;
+  int istart, jstart, kstart, iend, jend, kend;
+
+  istart = n_ghost;
+  iend   = nx-n_ghost;
+  if (ny > 1) {
+    jstart = n_ghost;
+    jend   = ny-n_ghost;
+  }
+  else {
+    jstart = 0;
+    jend   = ny;
+  }
+  if (nz > 1) {
+    kstart = n_ghost;
+    kend   = nz-n_ghost;
+  }
+  else {
+    kstart = 0;
+    kend   = nz;
+  }
+
+  // set initial values of conserved variables
+  for(k=kstart-1; k<kend; k++) {
+    for(j=jstart-1; j<jend; j++) {
+      for(i=istart-1; i<iend; i++) {
+
+        //get cell index
+        id = i + j*nx + k*nx*ny;
+
+        // Exclude the rightmost ghost cell on the "left" side
+        if ((k >= kstart) and (j >= jstart) and (i >= istart))
+        {
+          // set constant initial states
+          host_conserved[id] = rho;
+          host_conserved[1*n_cells+id] = rho*vx;
+          host_conserved[2*n_cells+id] = rho*vy;
+          host_conserved[3*n_cells+id] = rho*vz;
+          host_conserved[4*n_cells+id] = P/(gamma-1.0) + 0.5*rho*(vx*vx + vy*vy + vz*vz);
+          #ifdef DE
+          host_conserved[(n_fields-1)*n_cells+id] = P/(gamma-1.0);
+          #endif  // DE
+          #ifdef SCALAR
+          host_conserved[5*n_cells+id] = rho_d;
+          #endif // SCALAR
+        }
+      }
+    }
+  }
 }
--- cholla/dust_model/cuda/dust_model.h
+++ cholla/dust_model/cuda/dust_model.h
@@ -9,20 +9,22 @@ void Dust_Update(Real *dev_conserved, int nx, int ny, int nz, int n_ghost, int n
 
 __global__ void Dust_Kernel(Real *dev_conserved, int nx, int ny, int nz, int n_ghost, int n_fields, Real dt, Real gamma, Real *dt_array);
 
+void Conserved_Init(Real *host_conserved, Real rho, Real vx, Real vy, Real vz, Real P, Real rho_dust, Real gamma, int n_cells, int nx, int ny, int nz, int n_ghost, int n_fields);
+
 class Dust {
 
   public:
     Real T_, n_, dt_, d_gas_, d_dust_;
     Real tau_sp_;
-    Dust(Real T, Real n, Real dt, Real d_gas, Real d_dust) {
+    __device__ Dust(Real T, Real n, Real dt, Real d_gas, Real d_dust) {
       T_ = T;
       n_ = n;
       dt_ = dt;
       d_gas_ = d_gas;
       d_dust_ = d_dust;
     }
-    void set_tau_sp();
-    Real calc_dd_dt();
+    __device__ void set_tau_sp();
+    __device__ Real calc_dd_dt();
 
   private:
     Real YR_IN_S_ = 3.154*pow(10,7); // one year in s
