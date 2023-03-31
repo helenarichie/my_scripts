d6b712d549850c6dfb05a000b8c92ffaca4dd20d
helenarichie <helenarichie@gmail.com>
2023-03-31 18:43:27 -0400

    refactor code that extracts dense material at boundaries and change cutoff value for indexing

--- outflow.py
+++ outflow.py
@@ -3,13 +3,13 @@ from hconfig import *
 from csv import writer
 
 ######### hard-coded values! #########
-date = "2023-03-20"
+date = "2023-03-22"
 basedir = f"/ix/eschneider/helena/data/cloud_wind/{date}/"
 datadir = os.path.join(basedir, "hdf5/full/")
 pngdir = os.path.join(basedir, "png/")
 csvdir = os.path.join(basedir, "csv/")
 cat = True
-cutoff = 1/3*1e-24
+cutoff = 0.05*1e-24  # 5% of initial density
 ######################################
 
 data = ReadHDF5(datadir, fnum=0, nscalar=1, cat=cat)
@@ -23,52 +23,73 @@ indices = ["0", "-1"]
 fluids = ["gas", "dust"]
 
 def calc_mass_loss_rate(rho, v, area):
-        return np.sum(rho * v) * area
+        return np.sum(rho * np.abs(v)) * area
 
 if cat:
     files = glob.glob(os.path.join(datadir, "*.h5"))
 else:
     files = glob.glob(os.path.join(datadir, "*.h5.0"))
 
-f = open(os.path.join(csvdir, "outflow_cloud.csv"), "w")
+f = open(os.path.join(csvdir, "outflow_tot_cl.csv"), "w")
 f.close()
-f = open(os.path.join(csvdir, "flux_cloud.csv"), "w")
+f = open(os.path.join(csvdir, "outflow_dense_cl.csv"), "w")
 f.close()
-f = open(os.path.join(csvdir, "outflow_dust.csv"), "w")
+f = open(os.path.join(csvdir, "masses_cl.csv"), "w")
 f.close()
-f = open(os.path.join(csvdir, "flux_dust.csv"), "w")
+f = open(os.path.join(csvdir, "outflow_tot_dust.csv"), "w")
+f.close()
+f = open(os.path.join(csvdir, "outflow_dense_dust.csv"), "w")
+f.close()
+f = open(os.path.join(csvdir, "masses_dust.csv"), "w")
 f.close()
 
 def get_rates(d, cutoff):
-    rates, fluxes = np.zeros(6), np.zeros(6)
+    rates_tot, rates_dense, masses = np.zeros(6), np.zeros(6), np.zeros(6)
     for i, area in enumerate(areas):
         for index in indices:
             if (index == "0") and (i == 0):
-                rates[0] = calc_mass_loss_rate(d[0, :, :], data.vx_cgs()[0][0, :, :], area)
-                flux = d[0, :, :][d[0, :, :]>=cutoff]
-                fluxes[0] = np.sum(flux) * flux.size * dx**2
+                d_i, v_i = d[0, :, :], data.vx_cgs()[0][0, :, :]
+                mask = np.logical_and(d_i >= cutoff, v_i < 0) # mask for high-density, outflowing gas
+                rates_tot[0] = calc_mass_loss_rate(d_i, v_i, area)
+                rates_dense[0] = calc_mass_loss_rate(d_i[mask], v_i[mask], area)
+                masses[0] = np.sum(d_i[mask]) * d_i[mask].size * dx**2 # total density times volume of face
+
             if (index == "0") and (i == 1):
-                rates[1] = calc_mass_loss_rate(d[:, 0, :], data.vy_cgs()[0][:, 0, :], area)
-                flux = d[:, 0, :][d[:, 0, :]>=cutoff]
-                fluxes[1] = np.sum(flux) * flux.size * dx**2
+                d_i, v_i = d[:, 0, :], data.vy_cgs()[0][:, 0, :]
+                mask = np.logical_and(d_i >= cutoff, v_i < 0)
+                rates_tot[1] = calc_mass_loss_rate(d_i, v_i, area)
+                rates_dense[1] = calc_mass_loss_rate(d_i[mask], v_i[mask], area)
+                masses[1] = np.sum(d_i[mask]) * d_i[mask].size * dx**2
+
             if (index == "0") and (i == 2):
-                rates[2] = calc_mass_loss_rate(d[:, :, 0], data.vz_cgs()[0][:, :, 0], area)
-                flux = d[:, :, 0][d[:, :, 0]>=cutoff]
-                fluxes[2] = np.sum(flux) * flux.size * dx**2
+                d_i, v_i = d[:, :, 0], data.vz_cgs()[0][:, :, 0]
+                mask = np.logical_and(d_i >= cutoff, v_i < 0)
+                rates_tot[2] = calc_mass_loss_rate(d_i, v_i, area)
+                rates_dense[2] = calc_mass_loss_rate(d_i[mask], v_i[mask], area)
+                masses[2] = np.sum(d_i[mask]) * d_i[mask].size * dx**2
+
             if (index == "-1") and (i == 0):
-                rates[3] = calc_mass_loss_rate(d[-1, :, :], data.vx_cgs()[0][-1, :, :], area)
-                flux = d[-1, :, :][d[-1, :, :]>=cutoff]
-                fluxes[3] = np.sum(flux) * flux.size * dx**2
+                d_i, v_i = d[-1, :, :], data.vx_cgs()[0][-1, :, :]
+                mask = np.logical_and(d_i >= cutoff, v_i > 0)
+                rates_tot[3] = calc_mass_loss_rate(d_i, v_i, area)
+                rates_dense[3] = calc_mass_loss_rate(d_i[mask], v_i[mask], area)
+                masses[3] = np.sum(d_i[mask]) * d_i[mask].size * dx**2
+
             if (index == "-1") and (i == 1):
-                rates[4] = calc_mass_loss_rate(d[:, -1, :], data.vy_cgs()[0][:, -1, :], area)
-                flux = d[:, -1, :][d[:, -1, :]>=cutoff]
-                fluxes[4] = np.sum(flux) * flux.size * dx**2
+                d_i, v_i = d[:, -1, :], data.vy_cgs()[0][:, -1, :]
+                mask = np.logical_and(d_i >= cutoff, v_i > 0)
+                rates_tot[4] = calc_mass_loss_rate(d_i, v_i, area)
+                rates_dense[4] = calc_mass_loss_rate(d_i[mask], v_i[mask], area)
+                masses[4] = np.sum(d_i[mask]) * d_i[mask].size * dx**2
+
             if (index == "-1") and (i == 2):
-                rates[5] = calc_mass_loss_rate(d[:, :, -1], data.vz_cgs()[0][:, :, -1], area)
-                flux = d[:, :, -1][d[:, :, -1]>=cutoff]
-                fluxes[5] = np.sum(flux) * flux.size * dx**2
+                d_i, v_i = d[:, :, -1], data.vz_cgs()[0][:, :, -1]
+                mask = np.logical_and(d_i >= cutoff, v_i > 0)
+                rates_tot[5] = calc_mass_loss_rate(d_i, v_i, area)
+                rates_dense[5] = calc_mass_loss_rate(d_i[mask], v_i[mask], area)
+                masses[5] = np.sum(d_i[mask]) * d_i[mask].size * dx**2
     
-    return rates, fluxes
+    return rates_tot, rates_dense, masses
 
 for i in range(0, len(files)):
     data = ReadHDF5(datadir, fnum=i, nscalar=1, cat=cat)
@@ -78,26 +99,36 @@ for i in range(0, len(files)):
     gamma = head["gamma"]
     dx = data.dx_cgs()[0]
 
-    outflow_rates, fluxes = get_rates(data.d_cgs()[0], cutoff)
+    rates_tot, rates_dense, masses = get_rates(data.d_cgs()[0], cutoff)
 
-    with open(os.path.join(csvdir, "outflow_cloud.csv"), "a") as f:
+    with open(os.path.join(csvdir, "outflow_tot_cl.csv"), "a") as f:
         writer_obj = writer(f)
-        writer_obj.writerow(outflow_rates)
+        writer_obj.writerow(rates_tot)
         f.close()
 
-    with open(os.path.join(csvdir, "flux_cloud.csv"), "a") as f:
+    with open(os.path.join(csvdir, "outflow_dense_cl.csv"), "a") as f:
         writer_obj = writer(f)
-        writer_obj.writerow(fluxes)
+        writer_obj.writerow(rates_dense)
         f.close()
 
-    outflow_rates, fluxes = get_rates(conserved["scalar0"][0] * head["density_unit"], 1e-2*cutoff)
-
-    with open(os.path.join(csvdir, "outflow_dust.csv"), "a") as f:
+    with open(os.path.join(csvdir, "masses_cl.csv"), "a") as f:
         writer_obj = writer(f)
-        writer_obj.writerow(outflow_rates)
+        writer_obj.writerow(masses)
         f.close()
 
-    with open(os.path.join(csvdir, "flux_dust.csv"), "a") as f:
+    rates_tot, rates_dense, masses = get_rates(conserved["scalar0"][0] * head["density_unit"], 1e-2*cutoff)
+
+    with open(os.path.join(csvdir, "outflow_tot_dust.csv"), "a") as f:
         writer_obj = writer(f)
-        writer_obj.writerow(fluxes)
+        writer_obj.writerow(rates_tot)
+        f.close()
+
+    with open(os.path.join(csvdir, "outflow_dense_dust.csv"), "a") as f:
+        writer_obj = writer(f)
+        writer_obj.writerow(rates_dense)
+        f.close()
+
+    with open(os.path.join(csvdir, "masses_dust.csv"), "a") as f:
+        writer_obj = writer(f)
+        writer_obj.writerow(masses)
         f.close()
