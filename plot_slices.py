317bc3e84ba82ed6b3ad23a4114e17950ec2bad8
helenarichie <helenarichie@gmail.com>
2023-05-11 10:37:01 -0400

    clean up slice plotting script

--- plot_slices.py
+++ plot_slices.py
@@ -1,8 +1,16 @@
 from hconfig import *
 
 #################################
-date = "2023-03-22"
+date = "2023-05-09"
 cat = True
+vlims_gas = (-28, -22)
+vlims_dust = (-30, -20)
+vlims_T = ()
+spacing = 40 # pc
+fontsize = 20
+unit = "pc"
+plt.rcParams.update({'font.family': 'Helvetica'})
+plt.rcParams.update({'font.size': 20})
 #################################
 
 # directory with slices
@@ -15,8 +23,10 @@ head = data.head
 conserved = data.conserved
 
 nx = head["dims"][0]
-nz = head["dims"][-1]
+ny = head["dims"][-1]
 dx = head["dx"][0]
+if unit == "pc":
+    dx *= 1e3
 d_gas = data.d_cgs()
 d_dust = conserved["scalar0"] * head["density_unit"]
 vx = data.vx_cgs()
@@ -24,90 +34,71 @@ gamma = head["gamma"]
 t_arr = data.t_cgs() / yr_in_s
 T = data.T()
 
-wh_zero = np.where(d_dust<=0)
-d_dust[wh_zero] = 1e-40
-vlims_du = [np.log10(np.amin(d_dust.flatten())), np.log10(np.amax(d_dust.flatten()))]
-
-wh_zero = np.where(T<=0)
-T[wh_zero] = 1e-40
-#vlims_T = [np.log10(np.amin(T.flatten())), np.log10(np.amax(T.flatten()))]
-vlims_T = [2, 6]
-
 for i, d in enumerate(d_gas):
 
     fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(25,9))
     
-    # xy gas density projection
-    #im = axs[0][0].imshow(np.log10(d_gas[i].T), origin="lower", vmin=-28, vmax=-23, extent=[0, nx*dx, 0, nz*dx])
-    im = axs[0][0].imshow(np.log10(d_gas[i].T), origin="lower", extent=[0, nx*dx, 0, nz*dx])
-    ylabel = r'$\mathrm{log}_{10}(\rho)$ [$\mathrm{g}\mathrm{cm}^{-3}$]'
+    # xy gas density slice
+    im = axs[0][0].imshow(np.log10(d_gas[i].T), origin="lower", extent=[0, nx*dx, 0, ny*dx])
+    ylabel = r'$\mathrm{log}_{10}(\rho_{gas})$ [$\mathrm{g}\mathrm{cm}^{-3}$]'
     divider = make_axes_locatable(axs[0][0])
     cax = divider.append_axes("right", size="5%", pad=0.05)
     cbar = fig.colorbar(im, ax=axs[0][0], cax=cax)
-    cbar.set_label(ylabel)
-    axs[0][0].set_xticks(nx*dx*np.arange(0, 1.25, 0.25))
-    axs[0][0].set_yticks(nz*dx*np.arange(0, 1.25, 0.5))
+    cbar.set_label(ylabel, fontsize=fontsize)
+    axs[0][0].set_xticks(np.arange(0, nx*dx, spacing))
+    axs[0][0].set_yticks(np.arange(0, ny*dx, spacing))
     axs[0][0].tick_params(axis='both', which='both', direction='in', color='white', top=1, right=1)
-    axs[0][0].set_title(r"Gas Density Slice")
-    axs[0][0].set_xlabel(r"$x~$[kpc]")
-    #axs[0][0].plot([], [], " ", label="${:.1e}~yr$".format(t_arr[i]))
-    #axs[0][0].legend()
+    axs[0][0].set_title(r"Gas Density Slice", fontsize=fontsize)
+    axs[0][0].set_xlabel(r"$x~$[{}]".format(unit), fontsize=fontsize)
+    axs[0][0].set_ylabel(r"$y~$[{}]".format(unit), fontsize=fontsize)
+    axs[0][0].text(spacing, 0.1*dx*ny, f'{round(t_arr[i]/1e6, 2)} Myr', color='white', fontsize=fontsize)
     
 
-    # xy density-weighted temperature projection
-    im = axs[1][0].imshow(np.log10(d_dust[i].T), origin="lower", cmap="plasma", vmin=-28, vmax=-25, extent=[0, nx*dx, 0, nz*dx])
-    ylabel = r'$\mathrm{log}_{10}(\rho)$ [$\mathrm{g}\mathrm{cm}^{-3}$]'
+    # xy dust density
+    wh_zero = np.where(d_dust[i]<=0)
+    d_dust[i][wh_zero] = 1e-40
+
+    im = axs[1][0].imshow(np.log10(d_dust[i].T), origin="lower", cmap="plasma", vmin=vlims_dust[0], extent=[0, nx*dx, 0, ny*dx])
+    ylabel = r'$\mathrm{log}_{10}(\rho_{dust})$ [$\mathrm{g}\mathrm{cm}^{-3}$]'
     divider = make_axes_locatable(axs[1][0])
     cax = divider.append_axes("right", size="5%", pad=0.05)
     cbar = fig.colorbar(im, ax=axs[1][0], cax=cax)
-    cbar.set_label(ylabel)
-    axs[1][0].set_xticks(nx*dx*np.arange(0, 1.25, 0.25))
-    axs[1][0].set_yticks(nz*dx*np.arange(0, 1.25, 0.5))
+    cbar.set_label(ylabel, fontsize=fontsize)
+    axs[1][0].set_xticks(np.arange(0, nx*dx, spacing))
+    axs[1][0].set_yticks(np.arange(0, ny*dx, spacing))
     axs[1][0].tick_params(axis='both', which='both', direction='in', color='white', top=1, right=1)
-    axs[1][0].set_title(r"Dust Density Slice")
-    axs[1][0].set_xlabel(r"$x~$[kpc]")
-    axs[1][0].set_ylabel(r"$z~$[kpc]")
-    #axs[1][0].plot([], [], " ", label="${:.1e}~yr$".format(t_arr[i]))
-    #axs[1][0].legend()
+    axs[1][0].set_title(r"Dust Density Slice", fontsize=fontsize)
+    axs[1][0].set_xlabel(r"$x~$[{}]".format(unit), fontsize=fontsize)
+    axs[1][0].set_ylabel(r"$y~$[{}]".format(unit), fontsize=fontsize)
     
 
-    #im = axs[0][1].imshow(np.log10(T[i].T), origin="lower", cmap="inferno", vmin=2, vmax=vlims_T[1], extent=[0, nx*dx, 0, nz*dx])
-    im = axs[0][1].imshow(np.log10(T[i].T), origin="lower", cmap="inferno", extent=[0, nx*dx, 0, nz*dx])
-    
-    if np.isnan(np.log10(T[i].T).any()):
-        print("there's a nan")
-
-    ylabel = r'$\mathrm{log}_{10}(T) [\mathrm{K}]$'
+    # xy temperature slice
+    im = axs[0][1].imshow(np.log10(T[i].T), origin="lower", cmap="inferno", extent=[0, nx*dx, 0, ny*dx])
+    ylabel = r'$\mathrm{log}_{10}(T_{gas}) [\mathrm{K}]$'
     divider = make_axes_locatable(axs[0][1])
     cax = divider.append_axes("right", size="5%", pad=0.05)
     cbar = fig.colorbar(im, ax=axs[0][1], cax=cax)
-    cbar.set_label(ylabel)
-    axs[0][1].set_xticks(nx*dx*np.arange(0, 1.25, 0.25))
-    axs[0][1].set_yticks(nz*dx*np.arange(0, 1.25, 0.5))
+    cbar.set_label(ylabel, fontsize=fontsize)
+    axs[0][1].set_xticks(np.arange(0, nx*dx, spacing))
+    axs[0][1].set_yticks(np.arange(0, ny*dx, spacing))
     axs[0][1].tick_params(axis='both', which='both', direction='in', color='white', top=1, right=1)
-    axs[0][1].set_title(r"Temperature Slice")
-    axs[0][1].set_xlabel(r"$x~$[kpc]")
-    axs[0][1].set_ylabel(r"$z~$[kpc]")
-    #axs[0][1].plot([], [], " ", label="${:.1e}~yr$".format(t_arr[i]))
-    #axs[0][1].legend()
+    axs[0][1].set_title(r"Temperature Slice", fontsize=fontsize)
+    axs[0][1].set_xlabel(r"$x~$[{}]".format(unit), fontsize=fontsize)
+    axs[0][1].set_ylabel(r"$y~$[{}]".format(unit), fontsize=fontsize)
 
-    # xz velocity slice
-    # im = axs[1][1].imshow(vx[i].T, origin="lower", vmin=-1e-3, vmax=0.011, extent=[0, nx*dx, 0, nz*dx])
-    im = axs[1][1].imshow(vx[i].T*1e-5, origin="lower", extent=[0, nx*dx, 0, nz*dx])
-    ylabel = r'x-velocity [km/s]'
-
+    # xy velocity slice
+    im = axs[1][1].imshow(vx[i].T*1e-5, origin="lower", extent=[0, nx*dx, 0, ny*dx])
+    ylabel = r'$v_x$ [km/s]'
     divider = make_axes_locatable(axs[1][1])
     cax = divider.append_axes("right", size="5%", pad=0.05)
     cbar = fig.colorbar(im, ax=axs[1][1], cax=cax)
-    cbar.set_label(ylabel)
-    axs[1][1].set_xticks(nx*dx*np.arange(0, 1.25, 0.25))
-    axs[1][1].set_yticks(nz*dx*np.arange(0, 1.25, 0.5))
+    cbar.set_label(ylabel, fontsize=fontsize)
+    axs[1][1].set_xticks(np.arange(0, nx*dx, spacing))
+    axs[1][1].set_yticks(np.arange(0, ny*dx, spacing))
     axs[1][1].tick_params(axis='both', which='both', direction='in', color='white', top=1, right=1)
-    axs[1][1].set_title(r"x-velocity")
-    axs[1][1].set_xlabel(r"$x~$[kpc]")
-    axs[1][1].set_ylabel(r"$z~$[kpc]")
-    axs[1][1].plot([], [], " ", label="${:.1e}~yr$".format(t_arr[i]))
-    axs[1][1].legend()
+    axs[1][1].set_title(r"x-velocity slice", fontsize=fontsize)
+    axs[1][1].set_xlabel(r"$x~$[{}]".format(unit), fontsize=fontsize)
+    axs[1][1].set_ylabel(r"$y~$[{}]".format(unit), fontsize=fontsize)
 
     fig.tight_layout()
     
