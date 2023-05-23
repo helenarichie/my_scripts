18317ce0a7b05c039dfb8837ef05d1ff1b43f428
helenarichie <helenarichie@gmail.com>
2023-05-23 11:41:51 -0400

    add script to delete data

---
+++ delete_data.py
@@ -0,0 +1,23 @@
+import os
+import re
+import glob
+
+delete = True
+rename = True
+
+datadir = ""
+
+convert = lambda text: int(text) if text.isdigit() else text.lower()
+alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+
+if delete:
+    for i, filename in enumerate(sorted(glob.glob(os.path.join(datadir, "*.h5")), key=alphanum_key)):
+        # print(i)
+        # print(filename)
+        if i % 2 != 0:
+            os.remove(os.path.join(datadir, filename))
+            continue
+
+if rename: 
+    for i, filename in enumerate(sorted(glob.glob(os.path.join(datadir, "*.h5")), key=alphanum_key)):
+            os.rename(os.path.join(datadir, filename), os.path.join(datadir, str(i) + ".h5"))
