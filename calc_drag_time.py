fb598edf74931100029283fe2efb5b72c1604afe
helenarichie <helenarichie@gmail.com>
2023-07-07 10:23:50 -0400

    update script

--- calc_drag_time.py
+++ calc_drag_time.py
@@ -2,16 +2,44 @@
 # Use plasmapy to calculate Coulomb logarithm
 from hconfig import *
 e = 4.8032e-10  # cm^3/2 g^1/2 s^-1
-gamma = 5/2
-mu = 0.6
+gamma = 5/2  # specific heat ratio
+mu = 0.6  # mean molecular weight
 rho = 1e-24  # g/cm^3
-P = 1e6 * KB
+P = 1e6 * KB  # pressure
 v_rel = 1e3 * 1e5  # cm/s
-ln_coulomb = 8
+ln_coulomb = 8  # Coulomb logarithm
 z = 1  # ionization fraction, q/e
-Z_gr = 36
-a = 0.1  # micrometers
+Z_gr = 36  # grain charge
+a = 0.1  # grain radius, micrometers
 T = 1e7  # K
+
+# Hu et al. (2019) polynomial coefficients for non-thermal sputtering erosion rates
+a0_C, a1_C, a2_C, a3_C, a4_C, a5_C = -48.7, 55.7, -28.8, 7.44, -0.965, 0.005
+a0_Si, a1_Si, a2_Si, a3_Si, a4_Si, a5_Si = -32.4, 28.6, -11.6, 2.23, -0.208, 0.00736
+
+v_rel_arr = np.logspace(1, 5, 1000)
+
+yield_C = []
+yield_Si = []
+for v in np.log10(v_rel_arr):
+    yield_C.append(10**np.sum(a0_C + a1_C*v + a2_C*v**2 + a3_C*v**3 + a4_C*v**4 + a5_C*v**5))
+    yield_Si.append(10**np.sum(a0_Si + a1_Si*v + a2_Si*v**2 + a3_Si*v**3 + a4_Si*v**4 + a5_Si*v**5))
+
+yield_Si = np.array(yield_Si)
+
+wh_Y = np.argmin(np.abs(v_rel - v_rel_arr))
+Y_Si = yield_Si[wh_Y] 
+
+fig, ax = plt.subplots(figsize=(12,8))
+plt.loglog(v_rel_arr, yield_C, label="C", color="k", linewidth=3)
+plt.loglog(v_rel_arr, yield_Si, linestyle="--", label="Si", color="k", linewidth=3)
+plt.xlabel(r"$v_{rel}~[km\,s^{-1}]$")
+plt.ylabel(r"$Y~[\mu m\,yr^{-1}\,cm^3]$")
+plt.ylim(1e-9, 2e-5)
+plt.xlim(1e1, 1e5)
+plt.legend()
+# ax.tick_params(bottom=True, top=True, left=True, right=True)
+plt.savefig("/Users/helenarichie/Desktop/nonth_sputtering_yield.png")
 
 
 def sound_speed(P, rho):
@@ -43,35 +71,6 @@ t_drag = drag_timescale(a, rho/(mu*MP), T, G)
 
 print("Drag timescale: ", str(t_drag), " Myr")
 
-
-# Hu et al. (2019) polynomial coefficients for non-thermal sputtering erosion rates
-a0_C, a1_C, a2_C, a3_C, a4_C, a5_C = -48.7, 55.7, -28.8, 7.44, -0.965, 0.005
-a0_Si, a1_Si, a2_Si, a3_Si, a4_Si, a5_Si = -32.4, 28.6, -11.6, 2.23, -0.208, 0.00736
-
-v_rel_arr = np.logspace(1, 5, 1000)
-
-yield_C = []
-yield_Si = []
-for v in np.log10(v_rel_arr):
-    yield_C.append(10**np.sum(a0_C + a1_C*v + a2_C*v**2 + a3_C*v**3 + a4_C*v**4 + a5_C*v**5))
-    yield_Si.append(10**np.sum(a0_Si + a1_Si*v + a2_Si*v**2 + a3_Si*v**3 + a4_Si*v**4 + a5_Si*v**5))
-
-yield_Si = np.array(yield_Si)
-
-fig, ax = plt.subplots(figsize=(12,8))
-plt.loglog(v_rel_arr, yield_C, label="C", color="k", linewidth=3)
-plt.loglog(v_rel_arr, yield_Si, linestyle="--", label="Si", color="k", linewidth=3)
-plt.xlabel(r"$v_{rel}~[km\,s^{-1}]$")
-plt.ylabel(r"$Y~[\mu m\,yr^{-1}\,cm^3]$")
-plt.ylim(1e-9, 2e-5)
-plt.xlim(1e1, 1e5)
-plt.legend()
-# ax.tick_params(bottom=True, top=True, left=True, right=True)
-plt.savefig("/Users/helenarichie/Desktop/nonth_sputtering_yield.png")
-
-wh_Y = np.argmin(np.abs(v_rel - v_rel_arr))
-Y_Si = yield_Si[wh_Y] 
-
 def sputtering_timescale(a, n, Y):
     """Non-thermal sputtering timescale"""
     return 0.33 * a * (n**(-1)) * (Y/1e-6)**(-1)
@@ -79,9 +78,6 @@ def sputtering_timescale(a, n, Y):
 t_sp_nonth = sputtering_timescale(a, rho/(mu*MP), Y_Si)
 
 print("Nonth. sputtering timescale (Si): ", str(t_sp_nonth), " Myr")
-
-
-
 
 fig, ax = plt.subplots(figsize=(12,8))
 plt.semilogx(np.linspace(1e-9, np.amax(yield_Si), 10000), sputtering_timescale(a, rho/(mu*MP), np.linspace(1e-9, np.amax(yield_Si), 10000)), color="k", linewidth=3)
@@ -98,16 +94,19 @@ s = sonic_parameter(v_rel_arr, c_s)
 phi = Coulomb_potential(Z_gr, a, T)
 G = G_s(s, phi, z, ln_coulomb)
 drags = drag_timescale(a, rho/(mu*MP), T, G)
+sputs = sputtering_timescale(a, rho/(mu*MP), yield_Si) # yield_Si calculated from v_rel_arr
 
 fig, ax = plt.subplots(figsize=(12,8))
 ax.ticklabel_format(axis="y", style="sci")
-plt.semilogx(v_rel_arr, drags, color="k", linewidth=3)
+plt.semilogx(v_rel_arr, drags, color="k", linewidth=3, label="Drag")
+plt.semilogx(v_rel_arr, sputs, color="k", linestyle="--", linewidth=3, label="Sputtering (Si)")
 plt.xlabel(r"$v_{rel}~[km\,s^{-1}]$")
-plt.ylabel(r"$t_{drag}~[Myr]$")
-#plt.xlim(1e1, np.amax(yield_Si))
-#plt.ylim(np.amin(drags), np.amax(drags))
+plt.ylabel(r"$t~[Myr]$")
+plt.xlim(2e1, 1e5)
+plt.ylim(-0.1, 4)
 # ax.tick_params(bottom=True, top=True, left=True, right=True)
-plt.savefig("/Users/helenarichie/Desktop/drag_time_v.png")
+plt.legend()
+plt.savefig("/Users/helenarichie/Desktop/timescales_v.png")
 
 T_arr = np.logspace(1, 8, 1000)
 c_s = sound_speed(P, rho)
@@ -115,6 +114,7 @@ s = sonic_parameter(v_rel, c_s)
 phi = Coulomb_potential(Z_gr, a, T_arr)
 G = G_s(s, phi, z, ln_coulomb)
 drags = drag_timescale(a, rho/(mu*MP), T_arr, G)
+
 
 fig, ax = plt.subplots(figsize=(12,8))
 ax.ticklabel_format(axis="y", style="sci")
@@ -129,16 +129,40 @@ plt.savefig("/Users/helenarichie/Desktop/drag_time_T.png")
 n_arr = np.logspace(-2.5, 1, 1000)
 c_s = sound_speed(P, n_arr*mu*MP)
 s = sonic_parameter(v_rel, c_s)
-phi = Coulomb_potential(Z_gr, a, T_arr)
+phi = Coulomb_potential(Z_gr, a, T)
 G = G_s(s, phi, z, ln_coulomb)
-drags = drag_timescale(a, n_arr, T_arr, G)
+drags = drag_timescale(a, n_arr, T, G)
+sputs = sputtering_timescale(a, n_arr, Y_Si)
 
 fig, ax = plt.subplots(figsize=(12,8))
 ax.ticklabel_format(axis="y", style="sci")
-plt.semilogx(n_arr, drags, color="k", linewidth=3)
+plt.semilogx(n_arr, drags, color="k", linewidth=3, label="Drag")
+plt.semilogx(n_arr, sputs, color="k", linestyle="--", linewidth=3, label="Sputtering (Si)")
 plt.xlabel(r"$n~[cm^{-3}]$")
-plt.ylabel(r"$t_{drag}~[Myr]$")
+plt.ylabel(r"$t~[Myr]$")
 #plt.xlim(1e1, np.amax(yield_Si))
-#plt.ylim(np.amin(drags), np.amax(drags))
+#lt.ylim(-5, 100)
 # ax.tick_params(bottom=True, top=True, left=True, right=True)
-plt.savefig("/Users/helenarichie/Desktop/drag_time_n.png")
+plt.legend()
+plt.savefig("/Users/helenarichie/Desktop/timescales_n.png")
+
+
+a_arr = np.logspace(-5, 1, 1000)
+c_s = sound_speed(P, rho)
+s = sonic_parameter(v_rel, c_s)
+phi = Coulomb_potential(Z_gr, a_arr, T)
+G = G_s(s, phi, z, ln_coulomb)
+drags = drag_timescale(a_arr, rho/(mu*MP), T, G)
+sputs = sputtering_timescale(a_arr, rho/(mu*MP), Y_Si)
+
+fig, ax = plt.subplots(figsize=(12,8))
+ax.ticklabel_format(axis="y", style="sci")
+plt.semilogx(a_arr, drags, color="k", linewidth=3, label="Drag")
+plt.semilogx(a_arr, sputs, color="k", linestyle="--", linewidth=3, label="Sputtering (Si)")
+plt.xlabel(r"$a~[\mu m]$")
+plt.ylabel(r"$t~[Myr]$")
+#plt.xlim(1e1, np.amax(yield_Si))
+#lt.ylim(-5, 100)
+# ax.tick_params(bottom=True, top=True, left=True, right=True)
+plt.legend()
+plt.savefig("/Users/helenarichie/Desktop/timescales_a.png")
