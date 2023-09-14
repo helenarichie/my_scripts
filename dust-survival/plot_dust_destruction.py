f2f84282464971daa846f1a2ba7fd60794aef3cf
helenarichie <helenarichie@gmail.com>
2023-09-13 22:30:58 -0400

    plot the mass of destroyed dust

--- dust-survival/plot_dust_destruction.py
+++ dust-survival/plot_dust_destruction.py
@@ -5,8 +5,8 @@ from hconfig import *
 density_conversion = 5.028e-34/(3.24e-22)**3 # g/cm^3 to M_sun/kpc^3
 
 ##################################################################
-date = "2023-07-26"
-datestr = "072623"
+date = "2023-07-25"
+datestr = "0725"
 cat = True
 pad = 0.1
 spacing = 10  # pc, spacing of tick marks
@@ -58,11 +58,14 @@ mass_dust_tot = np.array(mass_dust_tot)
 
 mass_i = 0
 mass_out = []
+mass_lost = []
 for i, rate in enumerate(rate_dust):
     rate_i = np.sum(rate)
+    mass_lost.append(rate_i * dt)
     mass_i += rate_i * dt
     mass_out.append(mass_i)
 mass_out = np.array(mass_out)
+mass_lost = np.array(mass_lost)
 
 ymin = np.amin([np.amin(mass_dust_tot), np.amin(mass_out)]) - pad
 ymax = np.amax([np.amax(mass_dust_tot), np.amax(mass_out)]) + pad
@@ -72,21 +75,27 @@ xmax = np.amax(t_arr) + pad
 dx = None
 fig, axs = plt.subplots(nrows=1, ncols=1, figsize=(12, 10))
 
-axs.plot(t_arr/1e6, mass_dust_tot, linewidth=4, c="k")
-#axs[0][3].set_ylim(ymin, ymax)
+mass_destroyed = np.zeros(len(mass_lost))
+mass_destroyed[0] = 0
+mass_dest_cum = 0
+for i, mass in enumerate(mass_dust_tot):
+    if (i+1 < len(mass_lost)):
+        mass_dest_cum += (mass - mass_dust_tot[i+1] - mass_lost[i+1])
+        mass_destroyed[i+1] = mass_dest_cum
+
+axs.plot(t_arr/1e6, mass_dust_tot, linewidth=4, c="k", label="mass in volume")
+axs.plot(t_arr/1e6, mass_destroyed, linewidth=4, c="r", zorder=10, label="mass destroyed")
 axs.set_xlim(xmin/1e6, xmax/1e6)
-#axs[0][3].set_title("Total Mass")
 axs.set_xlabel("Time [Myr]")
 axs.set_ylabel(r"Dust Mass$~[M_\odot]$")
 axs.tick_params(axis='both', which='both', direction='in', color='black', top=1, right=1, length=9, width=2, reset=True)
-
-axs.plot(t_arr/1e6, mass_out, linewidth=4, linestyle="--", c="k")
-#axs[1][3].set_ylim(ymin, ymax)
+axs.plot(t_arr/1e6, mass_out, linewidth=4, linestyle="--", c="k", label="mass left volume")
 axs.set_xlim(xmin/1e6, xmax/1e6)
 axs.set_xlabel("Time [Myr]", fontsize=fontsize)
 axs.set_ylabel(r"Dust Mass$~[M_\odot]$", fontsize=fontsize)
 axs.set_xticks(np.linspace(0, np.amax(t_arr/1e6), 5).round(1))
 axs.set_yticks(np.linspace(0, np.amax(mass_dust_tot), 5).round(2))
+axs.legend(loc="upper right")
 
 fig.tight_layout()
 
