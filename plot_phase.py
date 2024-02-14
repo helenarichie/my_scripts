3f117ba42be7de9bacd93f150fc445fea522081b
helenarichie <helenarichie@gmail.com>
2024-02-14 14:28:33 -0500

    add phase diagram

---
+++ plot_phase.py
@@ -0,0 +1,140 @@
+from hconfig import *
+from csv import writer
+from matplotlib import colors
+from labellines import labelLines
+import seaborn as sns
+
+density_conversion = 5.028e-34/(3.24e-22)**3 # g/cm^3 to M_sun/kpc^3
+
+################# hard-coded, fill these in! ###################
+date = "2024-02-06"
+rho_cl_i = 1e-23  # n = 1, needed to index cloud material
+cutoff = rho_cl_i*density_conversion/3 # M_sun/kpc^3
+cat = True
+istart = 400
+iend = 400
+n_hydro = 1
+################################################################
+
+####################
+gas = True
+dust = False
+sputtering_contours = False
+a_grain = 1  # 0.1 micrometer
+n_hydro = 1
+tickwidth = 2
+nbins = 100
+#####################
+
+hist_cmap = sns.cubehelix_palette(light=1, as_cmap=True, reverse=True)
+
+d_min, d_max = None, None
+vmin, vmax = None, None
+if date == "2024-02-06":
+    d_min, d_max = 5e-28, 1e-22
+    T_min, T_max = 10, 7e7
+    if gas:
+        vmin, vmax = 5e-5, 5e2
+    if dust:
+        vmin, vmax = 5e-9, 0.05
+
+def tau_sp_n(T, tau_sp):
+    YR_IN_S = 3.154e7;
+    a1 = a_grain; # dust grain size in units of 0.1 micrometers
+    T_0 = 2e6; # K
+    omega = 2.5;
+    A = 0.17e9 * YR_IN_S; # Gyr in s
+
+    tau_sp *= yr_in_s
+
+    return A * 6e-4 * (a1/tau_sp) * ((T_0/T)**omega + 1)
+
+a = np.arange(0, 20, 2)
+a = np.array([0, 2, 4, 5, 6, 7, 8, 10, 12, 14, 16, 18])
+tau_sp = 10**a
+T_sput_i = np.linspace(T_min, T_max, 1000)
+T_sput = []
+n_sput = []
+tau_sps = []
+for tau in tau_sp:
+    tau_sps.append(np.linspace(tau, tau, 100))
+    T_sput.append(T_sput_i)
+    n_sput.append(tau_sp_n(T_sput_i, tau))
+tau_sps = np.array(tau_sps)
+n_sput = np.array(n_sput)
+
+basedir = f"/ix/eschneider/helena/data/cloud_wind/{date}/" # crc
+datadir = os.path.join(basedir, "hdf5/full/")
+pngdir = os.path.join(basedir, "png/phase/")
+
+# loop through hdf5 files to write out rates and masses for cloud, gas, and dust
+for i in range(istart, iend+1):
+    i *= n_hydro
+    # read in data
+    data = ReadHDF5(datadir, fnum=i, dust=True, cat=cat)
+    head = data.head
+    conserved = data.conserved
+    dx = head["dx"][0]
+    dx = data.dx_cgs()[0]
+
+    d = None
+    d_weight = None
+    cmap = None
+    if gas:
+        d = data.d_cgs()
+        d_weight = d
+        cmap = "viridis"
+    if dust:
+        d = data.d_cgs()
+        d_weight = conserved["dust_density"] * head["density_unit"]
+        wh_zero = np.where(d_weight<=0)
+        d_weight[wh_zero] = 1e-40
+        cmap = "plasma"
+
+    T = data.T()
+    t = data.t_cgs() / yr_in_s
+
+    d_sput = n_sput * (MP * 0.6)
+    weights = d_weight.flatten() * dx**3 * 5.02785e-34 # solar masses
+
+    log_d = np.log10(d.flatten())
+    log_T = np.log10(T.flatten())
+    extent = np.log10([[d_min, d_max], [T_min, T_max]])
+
+    fig, ax = plt.subplots(figsize=(10,7))
+    # hist = plt.hist2d(log_d, log_T, weights=weights,
+    #                   bins=150, norm=colors.LogNorm(),
+    #                   range=extent, cmap=hist_cmap, vmin=vmin, vmax=vmax)
+
+    hist = plt.hist2d(log_d, log_T, weights=weights,
+                      bins=nbins, norm=colors.LogNorm(),
+                      range=extent, cmap=hist_cmap)
+    
+    if sputtering_contours:
+        for j, tau in enumerate(tau_sp):
+            plt.plot(np.log10(d_sput[j]), np.log10(T_sput[j]), linestyle="--", 
+                    linewidth=2, color="lightgrey", label=r'$10^{{{:d}}}$ yr'.format(a[j]))
+
+        labelLines(plt.gca().get_lines(), zorder=2.5, fontsize=15)
+
+    plt.xlabel(r"$\log (\rho_{gas}~[g\,cm^{-3}])$")
+
+    cbar_label = None
+    if gas:
+        cbar_label = "Gas Mass $M_\odot$"
+    if dust:
+        cbar_label = "Dust Mass $M_\odot$"
+    plt.ylabel("$\log (T~[K])$")
+    plt.title(f"{round(t[0]/1e6, 1)} Myr", pad=10)
+    cbar = plt.colorbar()
+    cbar.set_label(cbar_label, rotation=270, labelpad=30)
+
+    cbar.ax.tick_params(length=9, width=tickwidth)
+    ax.tick_params(axis='both', which='both', direction='in', color='k', top=1, right=1, length=9, width=tickwidth)
+
+    if gas:
+        plt.savefig(pngdir + f"{i}_gas_rhoT.png", dpi=300)
+    if dust:
+        plt.savefig(pngdir + f"{i}_dust_rhoT.png", dpi=300)
+    
+    plt.close()
