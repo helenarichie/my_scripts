4b27dcac416bddbe078d7ee14a2aef270188557b
helenarichie <helenarichie@gmail.com>
2025-03-19 13:23:25 -0400

    update config settings

--- hconfig.py
+++ hconfig.py
@@ -1,17 +1,3 @@
-
-import sys
-if __file__ == "/ix/eschneider/helena/code/my_scripts/hconfig.py":
-    sys.path.insert(0, "/ix/eschneider/helena/code/analysis_scripts")
-if __file__ == "/Users/helenarichie/GitHub/my_scripts/hconfig.py":
-    sys.path.insert(0, "/Users/helenarichie/GitHub/analysis_scripts/")
-if __file__ == "/ccs/home/helenarichie/code/my_scripts/hconfig.py":
-    sys.path.insert(0, "/ccs/home/helenarichie/code/analysis_scripts")
-from read_hdf5 import ReadHDF5
-import os
-import glob
-import h5py
-
-import numpy as np
 import matplotlib.pyplot as plt
 
 plt.rcParams.update({'font.family': 'sans serif'})
@@ -28,19 +14,16 @@ plt.rcParams.update({'ytick.major.size': 7})
 plt.rcParams.update({'ytick.major.width': 1.5})
 plt.rcParams.update({'ytick.minor.size': 5})
 plt.rcParams.update({'ytick.minor.width': 2 })
-plt.rcParams.update({'lines.linewidth': 4})
+plt.rcParams.update({'lines.linewidth': 2})
+plt.rcParams.update({'xtick.top' : True})
+plt.rcParams.update({'ytick.right' : True})
 
-colors = ["lightskyblue", "violet", "forestgreen", "darkviolet", "thistle", "cadetblue", 
-            "palegoldenrod", "darksalmon", "indigo"]
 yr_in_s = 3.154e7 # s
 MP = 1.672622e-24 # g
 KB = 1.3807e-16 # cm^2 g s^-2 K^-1
 
 # import warnings
 # warnings.filterwarnings("ignore")
-from mpl_toolkits.axes_grid1 import make_axes_locatable
-import time
 
 plt.rcParams.update({'font.family': 'Helvetica'})
-plt.rcParams.update({'font.size': 10})
-# plt.style.use('dark_background')
+plt.rcParams.update({'font.size': 15})
